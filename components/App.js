8f2caec49883ed5b20198c1e02ba22a88ff13898
Kseniagg <gladkowaks@gmail.com>
2023-01-23 17:00:56 +0100

    first commit

---
+++ components/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Products from "./Products";
+
+function App() {
+    return (
+        <div className="App">
+            <Nav />
+            <Routes>
+                <Route path="/accueil" element={<Home />} />
+            </Routes>
+        </div>
+    );
+}
+
+export default App;

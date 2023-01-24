8bb1b9287dbd2093938c8036d2e18509a05421fe
gregnepos <neposgregory@gmail.com>
2023-01-24 10:32:36 +0000

    greg

--- components/App.js
+++ components/App.js
@@ -3,14 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Products from "./Products";
 
 function App() {
-    return (
-        <div className="App">
-            <Nav />
-            <Routes>
-                <Route path="/accueil" element={<Home />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+        <div> greg </div>
+        <Route path="/accueil" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
